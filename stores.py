ff31c95d10c2ac05d6507a5a3a6ce7aaf89e40f9
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-05-24 03:40:50 +0000

    Added database file of stores and item prices

---
+++ stores.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Store and prices dictionary"""
+
+
+SKIRT = {
+    'Macys': 30.99,
+    'H & M': 15.99,
+    'Zara': 49.99,
+    'Forever 21': 12.99
+    }
+
+DRESS = {
+    'Macys': 49.99,
+    'H & M': 19.99,
+    'Zara': 69.99,
+    'Forever 21': 15.99
+    }
+
+BLOUSE = {
+    'Macys': 29.99,
+    'H & M': 19.99,
+    'Zara': 39.99,
+    'Forever 21': 14.99
+    }
+
+PANTS = {
+    'Macys': 49.99,
+    'H & M': 29.99,
+    'Zara': 69.99,
+    'Forever 21': 24.99
+    }
+
+LEGGINGS = {
+    'Macys': 19.99,
+    'H & M': 15.99,
+    'Zara': 29.99,
+    'Forever 21': 10.99
+    }
